05ef4065497ccf80594ea74933e5b53ca7ceed44
Dave Lu <daveylupie@gmail.com>
2023-11-07 15:52:20 -0500

    moved colorbars and reworked heatmap display/save

--- scale.py
+++ scale.py
@@ -1,16 +1,16 @@
 import pandas as pd
-from sklearn.preprocessing import minmax_scale, robust_scale
+from sklearn.preprocessing import minmax_scale
 
-df = pd.read_csv("modified_data/fold_change.csv")
+df = pd.read_csv("modified_data/fold_change_no_outliers.csv")
 front = df.iloc[:, :6]
 df = df.iloc[:, 6:]
 biomarkers = list(df.columns)
 
 a = df.to_numpy()
-a = robust_scale(a, axis = 0)
+a = minmax_scale(a, axis = 0)   # scales data to a [0, 1] range
 
 df = pd.DataFrame(a, columns = biomarkers)
 
 df = pd.concat([front, df], axis = 1)
 
-df.to_csv("modified_data/temp.csv", index = False)
+df.to_csv("modified_data/scaled_fold_change_no_outliers.csv", index = False)
