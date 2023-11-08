d430181c93cf2757b3a3f85d9f235d8096adf803
Dave Lu <daveylupie@gmail.com>
2023-11-07 22:47:16 -0500

    increased space between blocks in corr. matrix

--- biomarker_correlation.py
+++ biomarker_correlation.py
@@ -9,12 +9,16 @@ primary_only = True
 
 #flag: 4 week biomarker data or 20 week biomarker data
 separate_weeks = True
-week = "20"
+week = "4"      # "4" or "20"
+
+save_dir = f"figures/correlation/correlation_change{week}wk_"
+if(primary_only): save_dir += "primary.png"
+else: save_dir += "both.png"
 
 if(primary_only):
     title_str = "Primary Group"
 else:
-    title_str = "Primary and Secondary Group"
+    title_str = "Primary and\nSecondary Group"
 
 plt.rcParams['text.usetex'] = True                                                      # enables LaTeX
 
@@ -40,9 +44,12 @@ mask = np.ones((num_biomarkers, num_biomarkers))
 mask = np.triu(mask)
 
 fig = sns.clustermap(corr_df, row_cluster = True, col_cluster = True, cmap = "seismic",
-                   xticklabels = True, yticklabels = True, linewidth = 0.5,
+                   xticklabels = True, yticklabels = True, linewidth = 1,
                    cbar_kws = {"label": r"Spearman's $\rho$"}, vmin = -1, vmax = 1,
-                   cbar_pos = [0.925, 0.2, 0.025, 0.6], figsize = (13, 14))
+                   cbar_pos = [0.85, 0.85, 0.05, 0.1], figsize = (14, 14))
+
+fig.ax_heatmap.set_xticklabels(fig.ax_heatmap.get_xmajorticklabels(), fontsize = 6)     # decrease font size
+fig.ax_heatmap.set_yticklabels(fig.ax_heatmap.get_ymajorticklabels(), fontsize = 6)
 
 # below section performs the masking AFTER the clustering is done
 # taken from: https://stackoverflow.com/questions/67879908/lower-triangle-mask-with-seaborn-clustermap
@@ -53,4 +60,5 @@ fig.ax_heatmap.collections[0].set_array(new_values)
 fig.figure.text(0.1, 0.9, title_str, fontsize = "xx-large",                            # add a title to the plot
                 horizontalalignment = "center", verticalalignment = "center")
 
+plt.savefig(save_dir, dpi=400)
 plt.show()
